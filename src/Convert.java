b871e2254df6275b90535afca3e375bac637cc2a
Samuel Rodriguez <samuelrodriguez@MacBook-Air-de-Samuel.local>
2026-01-01 21:32:23 -0300

    Initial commit

---
+++ src/Convert.java
@@ -0,0 +1,10 @@
+public class Convert {
+        public static void main (String[] args){
+            double temperaturaEmCelsius = 29.2;
+            double temperaturaEmFahrenheit =(temperaturaEmCelsius * 1.8) + 32;
+            String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+            System.out.println(mensagem);
+            int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+            System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+        }
+    }
